b579623c5e65d9ea5cb0a4d83bc965dfa10f92e5
pierdr <me@pierdr.com>
2017-05-04 23:14:11 -0700

    folder cleanup

---
+++ Pods/Target Support Files/Pods-airQualityDataDownloader/Pods-airQualityDataDownloader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_airQualityDataDownloader : NSObject
+@end
+@implementation PodsDummy_Pods_airQualityDataDownloader
+@end
---
+++ Pods/Target Support Files/Pods-airQualityDataDownloaderTests/Pods-airQualityDataDownloaderTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_airQualityDataDownloaderTests : NSObject
+@end
+@implementation PodsDummy_Pods_airQualityDataDownloaderTests
+@end
